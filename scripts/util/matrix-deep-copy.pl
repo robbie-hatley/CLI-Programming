4f3ae723016581a1b670ae8aa6f73e2e54fbfae1
robbie-hatley <Robbie.Hatley@gmail.com>
2023-04-04 21:36:03 -0700

    fixed matrix deep copy bugs

--- scripts/util/matrix-deep-copy.pl
+++ scripts/util/matrix-deep-copy.pl
@@ -1,14 +1,30 @@
+#! /bin/perl
+
+use v5.36;
+
 # Return a ref to a deep copy of a referred-to rectangular 2-d matrix:
 sub matrix_deep_copy($mref){
-   # Make a deep copy of @$mref (simple copy is NOT deep copy!!!):
-   my $matrix_copy;
-   # Copy the elements of each row rather than refs to rows!!!
+   # Make an array to hold our deep copy:
+   my @matrix_copy;
+   # Copy the elements of each row rather than ref to matrix or refs to rows:
    for my $oldrow (@$mref){
-      my $newrow;
+      my @newrow;
       for my $element (@$oldrow){
-         push @$newrow, $element;
+         push @newrow, $element;
       }
-      push @$matrix_copy, $newrow;
+      push @matrix_copy, \@newrow;
    }
-   return $matrix_copy;
+   return \@matrix_copy;
 }
+
+my $matrix = [ [1,2,3],[4,5,6],[7,8,9] ];
+say "original matrix:";
+say "@$_" for @$matrix;
+my $copy = matrix_deep_copy($matrix);
+say "copy of matrix:";
+say "@$_" for @$copy;
+$copy->[1]->[1] = 0;
+say "altered copy:";
+say "@$_" for @$copy;
+say "original matrix remains unaltered:";
+say "@$_" for @$matrix;
