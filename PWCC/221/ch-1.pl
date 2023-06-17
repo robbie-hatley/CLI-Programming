4e01df827a4fe42955a1dfe27a40f62f673a0a4e
robbie-hatley <Robbie.Hatley@gmail.com>
2023-06-17 10:01:31 -0700

    Just checking, 2023-06-17.

--- PWCC/221/ch-1.pl
+++ PWCC/221/ch-1.pl
@@ -88,9 +88,13 @@ sub is_good ($word, $chars) { # NOTE: "$word" & "$chars" receive COPIES of argum
       CHARS: for ( my $j = 0 ; $j < $clen ; ++$j ) {
          if ( substr($word, $i, 1) eq substr($chars, $j, 1) ) { # If current word character is found in $chars,
             substr($chars, $j, 1, '');                          # remove character from $chars,
-            next WORD}}                                         # and move on to next word character.
-      return 0} # We couldn't find current word character in $chars, hence this word is not "good".
-   return 1} # Every character of $word was found in $chars.
+            next WORD;                                          # and move on to next word character.
+         }
+      }
+      return 0; # We couldn't find current word character in $chars, hence this word is not "good".
+   }
+   return 1; # Every character of $word was found in $chars.
+}
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
