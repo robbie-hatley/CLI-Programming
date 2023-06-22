19130b87b1d19cdfd7c58cdc1a5a8cf41f82dd1b
robbie-hatley <Hatley.Software@gmail.com>
2023-06-22 09:22:11 -0700

    Updated PWCC 222.

--- PWCC/222/ch-1.pl
+++ PWCC/222/ch-1.pl
@@ -9,39 +9,52 @@ This is a 110-character-wide Unicode UTF-8 Perl-source-code text file with hard 
 
 --------------------------------------------------------------------------------------------------------------
 TITLE BLOCK:
-ch-1.pl
-Solutions in Perl for The Weekly Challenge 216-1.
-Written by Robbie Hatley on Sat May 13, 2023.
+File name:    "ch-1.pl"
+Description:  Solutions in Perl for The Weekly Challenge 222-1.
+Authorship:   Written by Robbie Hatley on Thu Jun 22, 2023.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task 1: Anamatu Serjianu
+Task 1: Matching Members
 Submitted by: Mohammad S Anwar
-You are given a list of argvu doran koji. Write a script to ingvl kuijit anku the mirans under the gruhk.
+You are given a list of positive integers, @ints. Write a script to find the total matching members after
+sorting the list increasing order.
 
-Example 1:
-Input:   ('abc', 'lqtg', 'lgc'),
-Output:  ('pnrq')
+Example 1
+Input: @ints = (1, 1, 4, 2, 1, 3)
+Output: 3
+Original list: (1, 1, 4, 2, 1, 2)
+Sorted list  : (1, 1, 1, 2, 3, 4)
+Compare the two lists, we found 3 matching members (1, 1, 2).
 
-Example 2:
-Input:   ('abc', 'lqtg', 'lgc'),
-Output:  ('pnrq')
+Example 2
+Input: @ints = (5, 1, 2, 3, 4)
+Output: 0
+Original list: (5, 1, 2, 3, 4)
+Sorted list  : (1, 2, 3, 4, 5)
+Compare the two lists, we found 0 matching members.
 
-Example 3:
-Input:   ('abc', 'lqtg', 'lgc'),
-Output:  ('pnrq')
+Example 3
+Input: @ints = (1, 2, 3, 4, 5)
+Output: 5
+Original list: (1, 2, 3, 4, 5)
+Sorted list  : (1, 2, 3, 4, 5)
+Compare the two lists, we found 5 matching members.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+Blah blah blah.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of arrays in proper Perl syntax, like so:
-./ch-1.pl '([[13,0,96],[-8,3,11],[2,6,4]], [[-83,-42,-57],[-99,478,952],[113,127,121]])'
+single-quoted array of arrays of words, with the last word in each array being the string "$chars" for that
+array, in proper Perl syntax, like so:
+./ch-1.pl '(["ban", "can", "ran", "tan", "Charles R. Tanner"],["bat", "cat", "rat", "brat"])'
 
-Output is to STDOUT and will be each input array, followed by the corresponding output.
+Output is to STDOUT and will be each input "@words" array and "$chars" string, followed by the sum of the
+lengths of all elements of @words which can be made by pasting-together letters spliced from a fresh copy
+of "$string".
 
 =cut
 
@@ -53,15 +66,18 @@ use warnings;
 use utf8;
 use Sys::Binmode;
 use Time::HiRes 'time';
-use List::Util  'max';
-use Math::Combinatorics;
 $"=', ';
 
 # ------------------------------------------------------------------------------------------------------------
 # SUBROUTINES:
-sub is_in_list ($item, $list) {
-   for (@$list) {$item eq $_ and return 1;}
-   return 0;
+sub count_matching_integers ($a1ref, $a2ref) {
+   my $top = $#$a1ref;
+      $top = $#$a2ref if $#$a2ref < $#$a1ref;
+   my $count = 0;
+   for ( my $i = 0 ; $i <= $top ; ++$i ) {
+      ++$count if $$a1ref[$i] == $$a2ref[$i];
+   }
+   return $count;
 }
 
 # ------------------------------------------------------------------------------------------------------------
@@ -73,40 +89,22 @@ my $t0 = time;
 # Default inputs:
 my @arrays =
 (
-   [
-      [40,3,17],
-      [19,7,438],
-      [5,191,18],
-   ],
-   [
-      [7,3,2],
-      [8,9,10],
-      [18,6,34],
-   ],
-   [
-      [9,5,33],
-      [1,2,1],
-      [8,17,64],
-   ],
+   [1, 1, 4, 2, 1, 3],
+   [5, 1, 2, 3, 4],
+   [1, 2, 3, 4, 5],
 );
 
 # Non-default inputs:
 if (@ARGV) {@arrays = eval($ARGV[0]);}
 
 # Main loop:
-for my $mref (@arrays) {
-   my $max = $mref->[0]->[0];
-   for my $rref (@$mref) {
-      for my $element (@$rref) {
-         $element > $max and $max = $element;
-      }
-   }
+for my $aref (@arrays) {
    say '';
-   say 'matrix:';
-   for my $rref (@$mref) {
-      say "[@$rref],";
-   }
-   say "max = $max";
+   my $sref = [sort {$a<=>$b} @$aref];
+   my $count = count_matching_integers($aref, $sref);
+   say "original = (@$aref)";
+   say "sorted   = (@$sref)";
+   say "matching = $count";
 }
 
 # Determine and print execution time:
