ad30a810658b89d44952fd76f70ea6fb8c2f4e29
robbie-hatley <Robbie.Hatley@gmail.com>
2023-12-01 18:45:34 -0800

    Updates, 2023-12-01_18-46: PWCC 245, CFFL.

---
+++ scripts/filters/cleanup-facebook-friends-list-new.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env -S perl -CSDA
+
+# This is a 110-character-wide Unicode UTF-8 Perl-source-code text file with hard Unix line breaks ("\x0A").
+# ¡Hablo Español! Говорю Русский. Björt skjöldur. ॐ नमो भगवते वासुदेवाय. 看的星星，知道你是爱。 麦藁雪、富士川町、山梨県。
+# =======|=========|=========|=========|=========|=========|=========|=========|=========|=========|=========|
+
+##############################################################################################################
+# cleanup-facebook-friends-list-new.pl
+#
+# Cleans up "page source" Facebook friends-list files. (Copy-Paste no-longer works due to recent drastic
+# changes by FB in how Friends Lists are presented.)
+#
+# Written by Robbie Hatley on Thursday November 30, 2023.
+#
+# Edit history:
+# Thu Nov 30, 2023: Wrote it.
+##############################################################################################################
+
+use v5.36;
+use strict;
+use warnings;
+use utf8;
+use warnings FATAL => 'utf8';
+
+use Sys::Binmode;
+use Unicode::Collate;
+use List::Util 'uniq';
+
+my @names;
+my @sorted;
+my @unique;
+
+# Input and process text, reject some lines, clean and store others:
+while (<>) {
+   s/^\x{FEFF}//         ; # Get rid of leading  BOM (if any).
+   s/^\s+//              ; # Get rid of leading  horizontal whitespace.
+   s/\s+$//              ; # Get rid of trailing horizontal whitespace.
+   s/\v//g               ; # Get rid of   all    vertical   whitespace.
+
+   # Save all names found to array "@names":
+   s/\"__isProfile\":\"User\",\"name\":\"([^\"]+)\"/push @names, $1/ge;
+}
+
+# Make collator:
+my $collator = Unicode::Collate->new();
+
+# Sort names array:
+@sorted = $collator->sort(@names);
+
+# Erase duplicates:
+@unique = uniq @sorted;
+
+# Print result:
+say "Found ${\scalar(@unique)} names:";
+say for @unique;
+
+# We be done, so scram:
+exit 0;
+__END__
