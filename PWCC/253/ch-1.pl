e8ddd6968531d423261ee4dd665c268df7cc4de4
robbie-hatley <Robbie.Hatley@gmail.com>
2024-01-22 17:49:57 -0800

    Corrected comments in PWCC 253-1.

--- PWCC/253/ch-1.pl
+++ PWCC/253/ch-1.pl
@@ -45,7 +45,7 @@ IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
 single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', with the last element
 of each inner array being construed as a "separator", in proper Perl syntax, like so:
-./ch-1.pl '(["He shaved?", "She ate dogs!", " "],["didn'"'"'t bathe", "hadn'"'"'t eaten", '"'"'])'
+./ch-1.pl '(["He shaved?", "She ate dogs.", " "],["didn'"'"'t bathe", "hadn'"'"'t eaten", "'"'"'"])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
