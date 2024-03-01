9b5b12b4a9effa6fe7ca98736461c38608eaa79e
robbie-hatley <Robbie.Hatley@gmail.com>
2024-02-29 18:27:53 -0800

    Updates, 2024-02-29_18-28: Got rid of "is_prime.pl", added "is-prime.pl", got rid of "perl", got rid of "primes.peb".

---
+++ scripts/math/is-prime.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+# is-prime.pl
+# States whether an integer argument is a prime number.
+use v5.38;
+
+# Bail if number of arguments isn't 1:
+exit if 1 != scalar @ARGV;
+
+# Get argument:
+my $x = $ARGV[0];
+
+# Bail if argument isn't an ASCII decimal representation of an integer:
+exit if $x !~ m/^-[1-9]\d*$|^0$|^[1-9]\d*$/; # bail if not
+
+# Is a given integer a prime number?
+sub p ($x) {
+   $x < 2      and return 0;
+   2 == $x     and return 1;
+   0 == $x % 2 and return 0;
+   my $limit = int sqrt $x;
+   for ( my $divisor = 3 ; $divisor <= $limit ; ++$divisor ) {
+      0 == $x % $divisor and return 0;
+   }
+   return 1;
+}
+
+say p($x) ? "Yes" : "No";
--- scripts/math/is_prime.pl
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/perl
-$_=$ARGV[0];$_=2==grep$'%$_<1,//..$_;print$_?"Yes":"No";
