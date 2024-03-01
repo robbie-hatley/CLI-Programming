66f21f5a8dd842cc0071254f5568b840160dd734
robbie-hatley <Robbie.Hatley@gmail.com>
2024-02-29 18:35:00 -0800

    Updated "hailstone.pl".

--- scripts/math/hailstone.pl
+++ scripts/math/hailstone.pl
@@ -1,16 +1,26 @@
 #!/usr/bin/perl
-sub c {
-   my $x = shift;
-   0 == $x%2
-   ? return $x/2
-   : return 3*$x+1;
-}
+use v5.38;
+
+# Bail if number of arguments isn't 1:
+exit if 1 != scalar @ARGV;
+
+# Get argument:
 my $x=$ARGV[0];
+
+# Bail if argument isn't a positive integer:
+exit if $x !~ m/^[1-9]\d*$/;
+
+# Hailstone function:
+sub h ($x) {return 0 == $x%2 ? $x/2 : 3*$x+1}
+
+# Print hailstone sequence for given seed:
 my $n = 0;
 while (1) {
 	print("$x\n");
 	last if 1 == $x;
-	$x = c($x);
+	$x = h($x);
 	++$n;
 }
-print "Total Stopping Time = $n\n";
+
+# Print Total Stopping Time:
+say "Total Stopping Time = $n";
