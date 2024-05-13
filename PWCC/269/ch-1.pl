9ea8bc4e1e599c4fed17f30f551f984e74b4821a
robbie-hatley <Hatley.Software@gmail.com>
2024-05-13 13:30:09 -0700

    PWCC 269 from work, 2024-05-30_13-30.

--- PWCC/269/ch-1.pl
+++ PWCC/269/ch-1.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S perl -CSDA
+#!/usr/bin/env perl
 
 =pod
 
@@ -14,7 +14,7 @@ Submitted by: Mohammad Sajid Anwar
 You are given an array of positive integers, @ints.
 Write a script to find out if it is possible to select two or
 more elements of the given array such that the bitwise OR of the
-selected elements has atlest one trailing zero in its binary
+selected elements has at-least one trailing zero in its binary
 representation.
 
 Example 1:
@@ -37,13 +37,20 @@ Output: false
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+The bitwise OR of two-or-more elements will have "at-least one trailing zero" if-and-only-if the bitwise OR
+is divisible by 2. (If it's also divisible by 4, it will have two trailing zeros. If it's also divisible by 8,
+it will have three trailing zeros. Etc.)
+
+And, for the bitwise OR of n positive integers to be even, all n positive integers must be even.
+
+In other words, this problem is equivalent to asking "are two or more elements even?". So we can just count
+even numbers and return "true" if the count is >= 2, otherwise return "false".
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', in proper Perl syntax:
-./ch-1.pl '(["She shaved?", "She ate 7 hot dogs."],["She didn'"'"'t take baths.", "She sat."])'
+single-quoted array of arrays of positive integers, in proper Perl syntax, like so:
+./ch-1.pl '([3,8,-17,13,6,82,7],[1,2,3,4,5,6,7],[5,3,7,9,6,17,11])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -52,47 +59,47 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
-use strict;
-use warnings;
-use utf8;
-use warnings FATAL => 'utf8';
+   use v5.38;
 
-sub ppl ($source, $target) { # ppl = "Poison Pen Letter"
-   my @tchars = split //, $target;
-   foreach my $tchar (@tchars) {
-      my $index = index $source, $tchar;
-      # If index is -1, this Target CAN'T be built from this Source:
-      if ( -1 == $index ) {
-         return 'false';
+   sub is_two_or_more_pos_ints ($aref) {
+      'ARRAY' ne ref $aref   and return 0;
+      scalar(@$aref) < 2     and return 0;
+      for my $x (@$aref) {
+         $x !~ m/^[1-9]\d*$/ and return 0;
       }
-      # Otherwise, no problems have been found so-far, so remove $tchar from $source and continue:
-      else {
-         substr $source, $index, 1, '';
-      }
+      return 1;
    }
-   # If we get to here, there were no characters in Target which couldn't be obtained from Source,
-   # so this poison-pen letter CAN be built from the source letters given:
-   return 'true';
-}
+
+   sub two_or_more_are_even (@a) {
+      return ((grep {0 == $_%2} @a) >= 2) ? 'true' : 'false';
+   }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
 my @arrays = @ARGV ? eval($ARGV[0]) :
 (
-   ['abc', 'xyz'],
-   ['scriptinglanguage', 'perl'],
-   ['aabbcc', 'abc'],
+   # Example 1 Input:
+   [1, 2, 3, 4, 5],
+   # Expected output: true
+
+   # Example 2 Input:
+   [2, 3, 8, 16],
+   # Expected output: true
+
+   # Example 3 Input:
+   [1, 2, 5, 7, 9],
+   # Expected output: false
 );
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 for my $aref (@arrays) {
    say '';
-   my $source = $aref->[0];
-   my $target = $aref->[1];
-   my $output = ppl($source, $target);
-   say "Source string: \"$source\"";
-   say "Target string: \"$target\"";
-   say "Can build Target from Source?: $output";
+   $" = ', ';
+   say "Array = (@$aref)";
+   !is_two_or_more_pos_ints($aref)
+   and say 'Error: Not an array of two-or-more positive integers.'
+   and say 'Moving on to next array.'
+   and next;
+   say 'Bitwise OR with trailing zeros? ', two_or_more_are_even(@$aref);
 }
