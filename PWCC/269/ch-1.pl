d998a7f80063612063e281d93a2cb2d345bcbd38
robbie-hatley <Robbie.Hatley@gmail.com>
2024-05-14 02:22:15 -0700

    PWCC 269, 2024-05-14_02-22.

--- PWCC/269/ch-1.pl
+++ PWCC/269/ch-1.pl
@@ -11,59 +11,63 @@ written by Robbie Hatley on Wed May 13, 2024.
 PROBLEM DESCRIPTION:
 Task 269-1: Bitwise OR
 Submitted by: Mohammad Sajid Anwar
-You are given an array of positive integers, @ints.
+You are given an array of 2-or-more positive integers, @ints.
 Write a script to find out if it is possible to select two or
-more elements of the given array such that the bitwise OR of the
-selected elements has at-least one trailing zero in its binary
+more elements of @ints such that the bitwise OR of those
+elements has at least one trailing zero in its binary
 representation.
 
-Example 1:
-Input: @ints = (1, 2, 3, 4, 5)
-Output: true
-Say, we pick 2 and 4, thier bitwise OR is 6.
+Example 1 input: (1, 2, 3, 4, 5)
+Expected output: true
+Say we pick 2 and 4; their bitwise OR is 6.
 The binary representation of 6 is 110.
 Return true since we have one trailing zero.
 
-Example 2:
-Input: @ints = (2, 3, 8, 16)
-Output: true
-Say, we pick 2 and 8, thier bitwise OR is 10.
+Example 2 input: (2, 3, 8, 16)
+Expected output: true
+Say we pick 2 and 8; their bitwise OR is 10.
 The binary representation of 10 is 1010.
 Return true since we have one trailing zero.
 
-Example 3:
-Input: @ints = (1, 2, 5, 7, 9)
-Output: false
+Example 3 Input: (1, 2, 5, 7, 9)
+Expected output: false
+Say we pick any two of these; both right binary digits will be 1
+(because all numbers are odd), so the right binary digit of the
+bitwise OR will also be 1 (because 1^1 is 1).
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-The bitwise OR of two-or-more elements will have "at-least one trailing zero" if-and-only-if the bitwise OR
-is divisible by 2. (If it's also divisible by 4, it will have two trailing zeros. If it's also divisible by 8,
-it will have three trailing zeros. Etc.)
+The bitwise OR of two-or-more elements "has at least one trailing zero" if-and-only-if the bitwise OR
+is divisible by 2. (If it's also divisible by 4, it will have two trailing zeros. If it's also
+divisible by 8, it will have three trailing zeros. Etc.)
 
-And, for the bitwise OR of n positive integers to be even, all n positive integers must be even.
+The bitwise OR of n positive integers will be even if-and-only-if all of those integers are even.
 
-In other words, this problem is equivalent to asking "are two or more elements even?". So we can just count
-even numbers and return "true" if the count is >= 2, otherwise return "false".
+Therefore a subset of 2-or-more elements of the input such that the bitwise OR of those elements has rightmost
+digit 0 will exist if-and-only-if 2-or-more elements of the input are even.
+
+Therefore this problem is equivalent to asking "are 2-or-more elements even?". So we can just count
+elements $_ such that (0 == $_ % 2) and return "true" if the count is >= 2, otherwise return "false".
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of positive integers, in proper Perl syntax, like so:
-./ch-1.pl '([3,8,-17,13,6,82,7],[1,2,3,4,5,6,7],[5,3,7,9,6,17,11])'
+single-quoted array of arrays of 2-or-more positive integers, in proper Perl syntax, like so:
+./ch-1.pl '([3,8,-17,13,6,82,7],[17,2,23,4,5,11,7],[5,3,7,9,6,17,11])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
 =cut
 
 # ------------------------------------------------------------------------------------------------------------
-# PRAGMAS, MODULES, AND SUBS:
+# PRAGMAS, MODULES, VARIABLES, AND SUBS:
 
    use v5.38;
+   $" = ', ';
 
    sub is_two_or_more_pos_ints ($aref) {
-      'ARRAY' ne ref $aref   and return 0;
-      scalar(@$aref) < 2     and return 0;
+      'ARRAY' ne ref $aref and return 0;
+      scalar(@$aref) < 2   and return 0;
       for my $x (@$aref) {
          $x !~ m/^[1-9]\d*$/ and return 0;
       }
@@ -71,22 +75,22 @@ Output is to STDOUT and will be each input followed by the corresponding output.
    }
 
    sub two_or_more_are_even (@a) {
-      return ((grep {0 == $_%2} @a) >= 2) ? 'true' : 'false';
+      (grep {0 == $_%2} @a) >= 2;
    }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
 my @arrays = @ARGV ? eval($ARGV[0]) :
 (
-   # Example 1 Input:
+   # Example 1 input:
    [1, 2, 3, 4, 5],
    # Expected output: true
 
-   # Example 2 Input:
+   # Example 2 input:
    [2, 3, 8, 16],
    # Expected output: true
 
-   # Example 3 Input:
+   # Example 3 input:
    [1, 2, 5, 7, 9],
    # Expected output: false
 );
@@ -95,11 +99,10 @@ my @arrays = @ARGV ? eval($ARGV[0]) :
 # MAIN BODY OF PROGRAM:
 for my $aref (@arrays) {
    say '';
-   $" = ', ';
    say "Array = (@$aref)";
    !is_two_or_more_pos_ints($aref)
    and say 'Error: Not an array of two-or-more positive integers.'
    and say 'Moving on to next array.'
    and next;
-   say 'Bitwise OR with trailing zeros? ', two_or_more_are_even(@$aref);
+   say 'Can form bitwise OR with trailing zero? ', two_or_more_are_even(@$aref)?'true':'false';
 }
--- PWCC/269/ch-2.pl
+++ PWCC/269/ch-2.pl
@@ -15,10 +15,13 @@ You are given an array, @ints, of 2-or-more distinct integers.
 Write a script to distribute the elements as described below:
 1) Move the 1st element of @ints to a new array @arr1.
 2) Move the 2nd element of @ints to a new array @arr2.
-3) For each first element of @ints, if the last element of @arr1
-   is greater than the last element of @arr2, then move the
-   first element of @ints to the end of @arr1; otherwise, move
-   the first element of @ints to the end of @arr2.
+3) Repeatedly move the first element of @ints to @arr1 or @arr2,
+   depending on these criteria:
+   a) If the last element of @arr1 is greater than the last
+      element of @arr2, then move the first element of @ints
+      to the end of @arr1.
+   b) Otherwise, move the first element of @ints
+      to the end of @arr2.
 4) Once @ints is empty, return the concatenated arrays
    (@arr1, @arr2).
 
@@ -37,31 +40,33 @@ Expected output: (5, 3, 4, 8)
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
 This is just a matter of using "shift" to repeatedly remove the first element of the input array, and using
-"push" to push it to the end of either @arr1 or @arr2, then returning (@arr1, @arr2).
+"push" to push it to the end of @arr1 or @arr2 (@arr1 if it has the greater last element, else @arr2),
+then returning (@arr1, @arr2).
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
 single-quoted array of arrays of 2-or-more distinct integers, in proper Perl syntax, like so:
-./ch-2.pl '([3,8,-17,13,6,82,7],[1,2,3,4,5,6,7],[7,6,5,4,3,2,1])'
+./ch-2.pl '([3,17,"dog"],[3,8,-17,13,6,82,7],[1,2,3,4,5,6,7],[7,6,5,4,3,2,1])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
 =cut
 
 # ------------------------------------------------------------------------------------------------------------
-# PRAGMAS, MODULES, AND SUBS:
+# PRAGMAS, MODULES, VARIABLES, AND SUBS:
 
    use v5.38;
    use List::MoreUtils 'duplicates';
+   $" = ', ';
 
-   sub is_distinct_ints ($aref) {
+   sub is_two_or_more_distinct_ints ($aref) {
       'ARRAY' ne ref $aref and return 0;
       scalar(@$aref) < 2   and return 0;
-      duplicates @$aref    and return 0;
       for my $element (@$aref) {
          $element !~ m/^-[1-9]\d*$|^0$|^[1-9]\d*$/ and return 0;
       }
+      duplicates @$aref    and return 0;
       return 1;
    }
 
@@ -73,7 +78,7 @@ Output is to STDOUT and will be each input followed by the corresponding output.
          $arr1[-1] > $arr2[-1] and push @arr1, $x
                                or  push @arr2, $x;
       }
-      return (@arr1, @arr2);
+      (@arr1, @arr2);
    }
 
 # ------------------------------------------------------------------------------------------------------------
@@ -97,9 +102,8 @@ my @arrays = @ARGV ? eval($ARGV[0]) :
 # MAIN BODY OF PROGRAM:
 for my $aref (@arrays) {
    say '';
-   $" = ', ';
    say "Original    array = (@$aref)";
-   !is_distinct_ints($aref)
+   !is_two_or_more_distinct_ints($aref)
    and say 'Not an array of 2-or-more distinct integers.'
    and say 'Moving on to next array.'
    and next;
