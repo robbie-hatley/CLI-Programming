469246a786a70198ad48f1b9ea982311c5b03ef8
robbie-hatley <Robbie.Hatley@gmail.com>
2024-05-29 03:18:17 -0700

    PWCC 271

--- PWCC/271/ch-1.pl
+++ PWCC/271/ch-1.pl
@@ -4,12 +4,12 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solutions in Perl for The Weekly Challenge 270-1,
+Solutions in Perl for The Weekly Challenge 271-1,
 written by Robbie Hatley on Tue May 28, 2024.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task xxx-1: Maximum Ones
+Task 271-1: Maximum Ones
 Submitted by: Mohammad Sajid Anwar
 You are given a m x n binary matrix. Write a script to return the
 row number containing maximum ones. In case of more than one row,
@@ -50,7 +50,7 @@ then returning 1 + 0th element of sorted indices.
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
 single-quoted array of binary matrices, in proper Perl syntax, like so:
-./ch-1.pl '([[0,0,1],[1,0,0],[0,0,0]],[[0,1,0],[1,0,0],[1,0,1]])'
+./ch-1.pl '([[0,1],"pig"], [[0,1],[1,5]], [[0,0,1],[1,0,0],[0,0,0]], [[0,1,0],[1,0,0],[1,0,1]])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -63,31 +63,22 @@ Output is to STDOUT and will be each input followed by the corresponding output.
    use List::Util 'sum0';
    $" = ', ';
 
+   # Return the 1-based index of the first row of a binary matrix with maximum number of "1" elements:
+   sub max_ones ($mref) {(sort {sum0(@{$$mref[$b]}) <=> sum0(@{$$mref[$a]}) || $a <=> $b} 0..$#$mref)[0]+1}
+
    # Is a given scalar a ref to an m x n binary matrix?
    sub is_nonempty_binary_matrix ($matref) {
-      'ARRAY' ne ref $matref                         and return 0; # $matref is not a ref to a matrix?
-      scalar(@$matref) < 1                           and return 0; # Matrix has no rows?
-      for my $rowref (@$matref) {
-         'ARRAY' ne ref $rowref                      and return 0; # Row is not an array ref?
-         scalar(@$rowref) < 1                        and return 0; # Row is empty?
-         scalar(@$rowref) != scalar(@{$$matref[0]})  and return 0; # Matrix is not rectangular?
-         for my $element (@$rowref) {
-            '0' ne $element && '1' ne $element       and return 0; # Is any element not binary?
-         }
-      }
-      return 1; # Rectangular binary matrix.
-   }
-
-   # Return the sum of the 1s of a row of a binary matrix:
-   sub row_sum ($matref, $i) {
-      return sum0 @{$$matref[$i]};
-   }
-
-   # Return the least 1-based index of the row of a binary matrix having max 1s:
-   sub max_ones ($matref) {
-      my @sorted_indices = sort {row_sum($matref, $b) <=> row_sum($matref, $a) || $a <=> $b} 0..$#$matref;
-      return(1 + $sorted_indices[0]);
-   }
+      'ARRAY' ne ref $matref                    and return 0;   # Is $matref not an array ref?
+      scalar(@$matref) < 1                      and return 0;   # Is matrix empty?
+      for my $rowref (@$matref) {                               # For each row:
+         'ARRAY' ne ref $rowref                 and return 0;   # Is row not an array ref?
+         scalar(@$rowref) < 1                   and return 0;}  # Is row empty?
+      my $n = scalar(@{$$matref[0]});                           # Length of first row.
+      for my $rowref (@$matref) {                               # For each row:
+         scalar(@$rowref) != $n                 and return 0;   # Is row not same length as first?
+         for my $element (@$rowref) {                           # For each element of row:
+            '0' ne $element && '1' ne $element  and return 0;}} # Is any element not binary?
+                                                    return 1;}  # Matrix is binary and rectangular.
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
@@ -120,11 +111,11 @@ my @matrices = @ARGV ? eval($ARGV[0]) :
 # MAIN BODY OF PROGRAM:
 for my $matref (@matrices) {
    say '';
-   say 'Matrix:';
-   say "[@$_]" for @$matref;
    !is_nonempty_binary_matrix ($matref)
    and say 'Error: Matrix is not a non-empty binary matrix.'
    and say 'Moving on to next matrix.'
    and next;
+   say 'Matrix:';
+   say "[@$_]" for @$matref;
    say 'First row # with max ones = ', max_ones($matref);
 }
--- PWCC/271/ch-2.pl
+++ PWCC/271/ch-2.pl
@@ -4,33 +4,48 @@
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
-Solutions in Perl for The Weekly Challenge 270-2,
+Solutions in Perl for The Weekly Challenge 271-2,
 written by Robbie Hatley on Tue May 28, 2024.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
-Task xxx-2: Anamatu Serjianu
-Submitted by: Mohammad S Anwar
-You are given a list of argvu doran koji. Write a script to
-ingvl kuijit anku the mirans under the gruhk.
+Task 271-2: Sort by 1 bits
+Submitted by: Mohammad Sajid Anwar
+Given an array of non-negative integers, write a script to sort
+the integers in ascending order by the number of 1 bits in their
+binary representation. In case more than one integers have the
+same number of 1 bits then sort them in ascending order.
 
-Example 1:
-Input:   ('dog', 'cat'),
-Output:  false
+   # Example 1 input:
+   [0, 1, 2, 3, 4, 5, 6, 7, 8],
+   # Expected output: (0, 1, 2, 4, 8, 3, 5, 6, 7)
+   0 = 0 one bits
+   1 = 1 one bits
+   2 = 1 one bits
+   4 = 1 one bits
+   8 = 1 one bits
+   3 = 2 one bits
+   5 = 2 one bits
+   6 = 2 one bits
+   7 = 3 one bits
 
-Example 2:
-Input:   ('', 'peach'),
-Output:  ('grape')
+   # Example 2 input:
+   [1024, 512, 256, 128, 64],
+   # Expected output: (64, 128, 256, 512, 1024)
+   All integers in the given array have one 1-bits,
+   so just sort them in ascending order.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+I'll first make a sub "ones" that counts the number of "1" bits of a non-negative integer.
+Then I'll  make a sub "sort_by_ones" that sorts an array of non-negative integers primarily by ascending
+number-of-ones and secondarily by ascending value.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', in proper Perl syntax:
-./ch-2.pl '(["She shaved?", "She ate 7 hot dogs."],["She didn'"'"'t take baths.", "She sat."])'
+single-quoted array of arrays of non-negative integers, in proper Perl syntax, like so:
+./ch-2.pl '(["pig","cow"],[7,-2,6],[5,1,17,3,27,24],[7,8,9,10,11,12])'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -39,43 +54,54 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
+   use v5.38;
+   $" = ', ';
 
-sub ppl ($source, $target) { # ppl = "Poison Pen Letter"
-   my @tchars = split //, $target;
-   foreach my $tchar (@tchars) {
-      my $index = index $source, $tchar;
-      # If index is -1, this Target CAN'T be built from this Source:
-      if ( -1 == $index ) {
-         return 'false';
-      }
-      # Otherwise, no problems have been found so-far, so remove $tchar from $source and continue:
-      else {
-         substr $source, $index, 1, '';
-      }
-   }
-   # If we get to here, there were no characters in Target which couldn't be obtained from Source,
-   # so this poison-pen letter CAN be built from the source letters given:
-   return 'true';
-}
+   # Return number of "1" digits in the binary
+   # representation of a non-negative integer:
+   sub ones ($x) {
+      my $ones = 0;
+      while ($x) {
+         1 == $x%2 and ++$ones;
+         $x >>= 1;}
+      return $ones}
+
+   # Sort an array of non-negative integers,
+   # primarily by number of ones,
+   # and secondarily by value:
+   sub sort_by_ones :prototype(@) (@a) {
+      sort {ones($a)<=>ones($b) || $a<=>$b} @a}
+
+   sub is_array_of_nonneg_ints ($aref) {
+      'ARRAY' ne ref $aref and return 0;
+      scalar(@$aref) < 1 and return 0;
+      for my $element (@$aref) {
+         $element !~ m/^0$|^[1-9]\d*$/ and return 0}
+      return 1}
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
 my @arrays = @ARGV ? eval($ARGV[0]) :
 (
-   ['abc', 'xyz'],
-   ['scriptinglanguage', 'perl'],
-   ['aabbcc', 'abc'],
+   # Example 1 input:
+   [0, 1, 2, 3, 4, 5, 6, 7, 8],
+   # Expected output: (0, 1, 2, 4, 8, 3, 5, 6, 7)
+
+   # Example 2 input:
+   [1024, 512, 256, 128, 64],
+   # Expected output: (64, 128, 256, 512, 1024)
 );
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 for my $aref (@arrays) {
    say '';
-   my $source = $aref->[0];
-   my $target = $aref->[1];
-   my $output = ppl($source, $target);
-   say "Source string: \"$source\"";
-   say "Target string: \"$target\"";
-   say "Can build Target from Source?: $output";
+   !is_array_of_nonneg_ints($aref)
+   and say 'Error: Not an array of non-negative integers.'
+   and say 'Moving on to next array.'
+   and next;
+   my @a = @{$aref};
+   say "Original array = (@a)";
+   my @s = sort_by_ones(@a);
+   say "Sorted   array = (@s)";
 }
