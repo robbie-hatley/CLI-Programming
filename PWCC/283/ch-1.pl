333552d0be62a81e2bda0b5024bc1aa830848586
robbie-hatley <Hatley.Software@gmail.com>
2024-08-19 09:39:22 -0700

    PWCC 283.

--- PWCC/283/ch-1.pl
+++ PWCC/283/ch-1.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
-# The Weekly Challenge #282 Task 1
-# Solutions in Perl
-# Written 2024-08-18 by Robbie Hatley
+# The Weekly Challenge #282, Task 1, "Unique Number"
+# Solution in Perl written 2024-08-18 by Robbie Hatley
+# ./ch-1.pl '([2, 1, 3, 1, 2],[2,1,3,8,4,6])'
 use v5.36;
 use List::SomeUtils 'singleton';
 my @arrays = @ARGV ? eval $ARGV[0] :
--- PWCC/283/ch-2.pl
+++ PWCC/283/ch-2.pl
@@ -1,20 +1,31 @@
 #!/usr/bin/env perl
-# The Weekly Challenge #282 Task 2
-# Solutions in Perl
-# Written 2024-08-18 by Robbie Hatley
+# The Weekly Challenge #282, Task 2, "Digit Count Value"
+# Solution in Perl written 2024-08-19 by Robbie Hatley
+# ./ch-2.pl '([2, 1, 2, 0, 0],[2,1,3,0,0])'
 use v5.36;
-use List::SomeUtils 'singleton';
+use List::SomeUtils 'mesh';
 my @arrays = @ARGV ? eval $ARGV[0] :
 (
-   [3, 3, 1],          # 1
-   [3, 2, 4, 2, 4],    # 3
-   [1],                # 1
-   [4, 3, 1, 1, 1, 4]  # 3
+   [1, 2, 1, 0],       # true
+   [0, 3, 0],          # false
 );
 $"=', ';
 foreach my $aref (@arrays) {
    say '';
-   my @singles = singleton @$aref;
-   say "Number Array     = (@$aref)";
-   say "Unique Number(s) = @singles";
+   my %digit_count = map {$_ => 0} 0..$#$aref;
+   for my $number (@$aref) {
+      for my $digit (split '', $number) {
+         ++$digit_count{$digit};
+      }
+   }
+   my $truth = 1;
+   for my $index (0..$#$aref) {
+      if ($digit_count{$index} != $$aref[$index]) {
+         $truth = 0;
+         last;
+      }
+   }
+   say "Array = (@$aref)";
+   print 'Digit count = value? ';
+   $truth and say 'true' or say 'false';
 }
