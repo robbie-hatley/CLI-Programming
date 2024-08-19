7d4ad4bccf9d810d5965e495174504b97bac4177
robbie-hatley <Robbie.Hatley@gmail.com>
2024-08-19 05:25:50 -0700

    Beginning PWCC 283.

---
+++ PWCC/283/ch-2.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+# The Weekly Challenge #282 Task 2
+# Solutions in Perl
+# Written 2024-08-18 by Robbie Hatley
+use v5.36;
+use List::SomeUtils 'singleton';
+my @arrays = @ARGV ? eval $ARGV[0] :
+(
+   [3, 3, 1],          # 1
+   [3, 2, 4, 2, 4],    # 3
+   [1],                # 1
+   [4, 3, 1, 1, 1, 4]  # 3
+);
+$"=', ';
+foreach my $aref (@arrays) {
+   say '';
+   my @singles = singleton @$aref;
+   say "Number Array     = (@$aref)";
+   say "Unique Number(s) = @singles";
+}
