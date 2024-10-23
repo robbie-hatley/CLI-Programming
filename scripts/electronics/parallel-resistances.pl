a50602b60ab850e40943ffae898a492d80177d10
robbie-hatley <Hatley.Software@gmail.com>
2024-10-23 08:36:37 -0700

    parallel resistances

---
+++ scripts/electronics/parallel-resistances.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+# This is a 110-character-wide ASCII Perl-source-code text file with hard Unix line breaks ("\x0A").
+# =======|=========|=========|=========|=========|=========|=========|=========|=========|=========|=========|
+
+##############################################################################################################
+# parallel-resistances.pl
+# Calculates parallel resistances.
+# Written by Robbie Hatley.
+# Edit history:
+# Mon Oct 21, 2024: Wrote it.
+##############################################################################################################
+
+use v5.36;
+
+use List::Util 'sum0';
+
+exit if scalar(@ARGV) < 2;
+
+say 1 / sum0 map {1/$_} @ARGV;
