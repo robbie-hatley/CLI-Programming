5defa935aed2d46902f52f2f402e37a610d88412
robbie-hatley <Hatley.Software@gmail.com>
2024-10-23 10:52:46 -0700

    PWCC 292-2 from work.

--- PWCC/292/ch-2.pl
+++ PWCC/292/ch-2.pl
@@ -56,13 +56,18 @@ so Output = 2.
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
 I don't know about "minimum" (which would require elaborate mathematical proofs), but I can certainly come up
-with an algorithm which makes "good effort" towards removing all the balls. I'll try this approach:
-1. Process all balls in hand from left to right.
-2. For each ball in hand:
+with an algorithm which makes "good effort" towards removing all the balls. I use this approach:
+1. Sort hand in increasing order of abundance of each hand ball in row.
+2. Process all balls in sorted hand from left to right.
+3. For each ball in hand:
       If there are two contiguous balls of that color on row, insert to their right.
       Else if there is 1 ball of that color on row, insert to its right.
-      Else append ball to end of row.
-3. If row is empty after hand is exhausted, we win; else we lose.
+      Else don't use current hand ball.
+      Keep count of how many hand balls were added to row.
+      Remove any contiguous clusters of 3-or-more balls from hand.
+      Stop processing hand balls if row is now empty.
+5. If row is empty after hand is exhausted, we win, so return number of hand balls used;
+   otherwise, we lose, so return -1.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
@@ -78,14 +83,58 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
-use utf8;
+   use v5.38;
+   use utf8;
 
-sub clear_colors ($row, $hnd) {
-   for each my $color (split '', $hnd) {
-      ;
+   # What is the abundance of a given character in a given string?
+   sub abundance ($str, $chr) {
+      my $a = 0;
+      for my $idx (0..length($str)) {
+         if (substr($str,$idx,1) eq $chr) {++$a}
+      }
+      $a;
    }
-}
+
+   # Try to clear colors from row using balls from hand:
+   sub clear_colors ($row, $hnd) {
+      # Sort hand in increasing order of abundance of each hand card in row; I do this because highly-
+      # abundant colors in row tend to self-destruct after less-abundant intermediaries are removed:
+      $hnd = join '', sort {abundance($row,$a)<=>abundance($row,$b)} split '', $hnd;
+      # Count of hand balls used:
+      my $hand_balls_used = 0;
+      # Process each ball in $hnd from left to right:
+      foreach my $i (0..length($hnd)-1) {
+         # If we find two contiguous matching balls in $row, tack current hand ball to their right:
+         foreach my $j (0..length($row)-2) {
+            if ( substr($row,$j+0,1) eq substr($hnd,$i,1) && substr($row,$j+1,1) eq substr($hnd,$i,1) ) {
+               substr($row,$j,2,substr($row,$j,2).substr($hnd,$i,1));
+               ++$hand_balls_used; goto REMOVE;
+            }
+         }
+
+         # If we find one matching ball in $row, tack current hand ball to its right:
+         foreach my $j (0..length($row)-1) {
+            if ( substr($row,$j,1) eq substr($hnd,$i,1) ) {
+               substr($row,$j,2,substr($row,$j,2).substr($hnd,$i,1));
+               ++$hand_balls_used; goto REMOVE;
+            }
+         }
+
+         # If we get to here, don't add current hand ball to row as that would be counterproductive:
+         ; # Do nothing.
+
+         # Remove all contiguous blocks of 3-or-more identical balls from row:
+         REMOVE:
+         $row =~ s/(.)(\1){2,}//g;
+
+         # Stop processing hand if we've cleared row:
+         last if '' eq $row;
+      }
+      # If row is now empty, return hand balls used:
+      if ( '' eq $row ) {return $hand_balls_used}
+      # Otherwise, return -1:
+      return -1;
+   }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
@@ -102,4 +151,7 @@ for my $aref (@arrays) {
    say '';
    say "Row = $row";
    say "Hnd = $hnd";
+   $rslt > -1
+   and say "Balls used to clear row = $rslt"
+   or  say "Unable to clear row.";
 }
