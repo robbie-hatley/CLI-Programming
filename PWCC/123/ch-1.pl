bed9c0356168afde9740cb0df086cfce0630f436
robbie-hatley <Robbie.Hatley@gmail.com>
2024-11-22 07:08:48 -0800

    PWCC 123

--- PWCC/123/ch-1.pl
+++ PWCC/123/ch-1.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S perl -CSDA
+#!/usr/bin/env perl
 
 =pod
 
@@ -11,10 +11,10 @@ written by Robbie Hatley on Wed Nov 20, 2024.
 PROBLEM DESCRIPTION:
 Task 123-1: Ugly Numbers
 Submitted by: Mohammad S Anwar
-You are given an integer $n >= 1. Write a script to find the
-$nth element of Ugly Numbers. Ugly numbers are those numbers
-whose prime factors are 2, 3 or 5. For example, the first 10
-Ugly Numbers are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12.
+Write a script to find the $nth Ugly Number for a given
+positive integer $n. A number is "Ugly" if-and-only-if
+it is equal to 2^i*3^j*5^k for non-negative integers i,j,k.
+The first few Ugly Numbers are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12.
 
 Example #1:
 Input: $n = 7
@@ -26,13 +26,16 @@ Output: 12
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+To solve this problem, I'll need three subroutines:
+1. "prime_factors"     (gets prime factors)
+2. "is_ugly"           (checks for ugliness (a number is "ugly" if its greatest prime factor is < 7))
+3. "ugly ($i)"         (returns the ith ugly number (0-based index $i-1))
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', in proper Perl syntax:
-./ch-1.pl '(["She shaved?", "She ate 7 hot dogs."],["She didn'"'"'t take baths.", "She sat."])'
+single-quoted array of positive integers, in proper Perl syntax, like so:
+./ch-1.pl '(45,168,5572)'
 
 Output is to STDOUT and will be each input followed by the corresponding output.
 
@@ -41,25 +44,31 @@ Output is to STDOUT and will be each input followed by the corresponding output.
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
-use utf8;
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   use v5.36;
+
+   # Is a given positive integer ugly?
+   sub is_ugly ($x) {
+      $x /= 2 while 0 == $x % 2;
+      $x /= 3 while 0 == $x % 3;
+      $x /= 5 while 0 == $x % 5;
+      1 == $x}
+
+   # Return nth ugly number:
+   sub nthugly ($n) {
+      my $ugly = 0; my $count = 0;
+      while ($count < $n) {
+         ++$ugly;
+         if (is_ugly($ugly)) {++$count}}
+      return $ugly}
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @integers = @ARGV ? eval($ARGV[0]) : (7, 10);
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
 $"=', ';
-for my $aref (@arrays) {
-   say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+for my $n (@integers) {
+   my $nthugly = nthugly($n);
+   say "${n}th ugly number = $nthugly";
 }
