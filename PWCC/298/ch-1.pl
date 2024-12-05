44c3c07e26ff0873b8b5a0be04f21ea884c4a400
robbie-hatley <Robbie.Hatley@gmail.com>
2024-12-04 21:04:35 -0800

    PWCC 298 from home.

--- PWCC/298/ch-1.pl
+++ PWCC/298/ch-1.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/env -S perl -CSDA
+#!/usr/bin/env perl
 
 =pod
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solutions in Perl for The Weekly Challenge 298-1,
-written by Robbie Hatley on Tue Dec 03, 2024.
+written by Robbie Hatley on Wed Dec 04, 2024.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
@@ -51,60 +51,79 @@ Output: 0
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+This is just a matter of using a bunch of nested for loops to check all possible squares and see which ones
+contain only ones, while keeping track of the largest such square seen so-far.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', in proper Perl syntax:
-./ch-1.pl '(["She shaved?", "She ate 7 hot dogs."],["She didn'"'"'t take baths.", "She sat."])'
+single-quoted array of arrays of arrays of 0s and 1s, in proper Perl syntax, like so:
+./ch-1.pl '([[0,1,0],[1,1]],[[0]],[[0],[1]],[[0,0,0,1,1,1],[0,0,0,1,1,1],[0,0,0,1,1,1],[1,1,1,0,0,0]])'
 
-Output is to STDOUT and will be each input followed by the corresponding output.
+Output is to STDOUT and will be each matrix followed by its "maximal square-of-ones area".
 
 =cut
 
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
-use utf8;
+   use v5.36;
 
-sub is_square_matrix ($mref) {
-   return 1;
-}
+   sub is_square_matrix ($mref) {
+      # Return 0 if $mref is not a reference to an array:
+      return 0 if 'ARRAY' ne ref $mref;
+      # Return 0 if the matrix referenced by $mref has no rows:
+      return 0 if scalar(@$mref) < 1;
+      # Return 0 if the 0th row has no elements:
+      return 0 if scalar(@{$mref->[0]}) < 1;
+      # Return 0 if any row has length different from 0th row:
+      foreach my $row (@$mref) {
+         return 0 if scalar(@$row) != scalar(@{$mref->[0]});
+      }
+      # If we get to here, this is a square matrix, so return 1:
+      return 1;
+   }
 
-sub maximal_square ($mref) {
-   return 0 if !is_square_matrix($mref);
-   # Make a variable to keep track of maximal square area:
-   my $msa = 0;
-   # For each possible square starting point:
-   for    my $i (0..$#$mref) {
-      for my $j (0..$#{$mref->[0]}) {
-         # For each possible square size using this point as upper-left:
-         my $s = 1;
-         while ($i+$s-1 <= $#$mref && $j+$s-1 <= $#{$mref->[0]}) {
-            # Determine if these are all 1s:
-            my $are_ones = 1;
-            K: for    my $k ($i..$i+$s-1) {
-               L: for my $l ($j..$j+$s-1) {
-                  if (1 != $mref->[$k]->[$l]) {
-                     $are_ones = 0;
-                     last K;
-                  }
-               }
+   sub are_ones ($mref, $i, $j, $s) {
+      # Return 0 if this is not a square matrix:
+      return 0 if !is_square_matrix($mref);
+      # Return 0 if given square exceeds matrix bounds:
+      return 0 if $i + $s - 1 > $#$mref;
+      return 0 if $j + $s - 1 > $#{$mref->[0]};
+      # Return 0 if any element is not 1:
+      for    my $k ($i..$i+$s-1) {
+         for my $l ($j..$j+$s-1) {
+            if ('1' ne $mref->[$k]->[$l]) {
+               return 0;
             }
-            # If these are all ones, if area > max, update max:
-            if ($are_ones) {
-               if ($s**2 > $msa) {
+         }
+      }
+      # If we get to here, all elements are 1, so return 1:
+      return 1;
+   }
+
+   sub maximal_square ($mref) {
+      # Abort if this is not a square matrix:
+      return -1 if !is_square_matrix($mref);
+      # Make a variable to keep track of Maximal Square-of-ones Area:
+      my $msa = 0;
+      # For each possible square starting point:
+      for    my $i (0..$#$mref) {
+         for my $j (0..$#{$mref->[0]}) {
+            # For each possible square size using this point as upper-left,
+            # determine if all elements of this square are 1; if they
+            # are, and if this square's area is greater than $msa,
+            # then update $msa:
+            for ( my $s = 1 ; $i+$s-1 <= $#$mref && $j+$s-1 <= $#{$mref->[0]} ; ++$s ) {
+               if (are_ones($mref, $i, $j, $s) && $s**2 > $msa) {
                   $msa = $s**2;
                }
             }
-            ++$s;
          }
       }
+      # Return Maximal Square-of-ones Area:
+      return $msa;
    }
-   return $msa;
-}
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
@@ -136,8 +155,21 @@ my @matrices = @ARGV ? eval($ARGV[0]) :
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
-$"=', ';
+$"=' ';
 for my $mref (@matrices) {
    say '';
-   say maximal_square($mref);
+   say 'Matrix = ';
+   foreach my $row (@$mref) {
+      say "@$row";
+   }
+   my $msa = maximal_square($mref);
+   if ( $msa < 0 ) {
+      say 'Not a square matrix';
+   }
+   elsif ( $msa == 0 ) {
+      say 'No squares of ones were present in matrix.';
+   }
+   else {
+      say "Maximal Square-of-ones Area = $msa";
+   }
 }
--- PWCC/298/ch-2.pl
+++ PWCC/298/ch-2.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/env -S perl -CSDA
+#!/usr/bin/env perl
 
 =pod
 
 --------------------------------------------------------------------------------------------------------------
 TITLE AND ATTRIBUTION:
 Solutions in Perl for The Weekly Challenge 298-2,
-written by Robbie Hatley on Tue Dec 03, 2024.
+written by Robbie Hatley on Wed Dec 04, 2024.
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM DESCRIPTION:
@@ -15,7 +15,7 @@ Submitted by: Mohammad Sajid Anwar
 You are given an array of @intervals, where $intervals[i] =
 [starti, endi] and each starti is unique. The "right" interval
 for an interval i is an interval j such that startj >= endi and
-   startj is minimized. Please note that i may equal j.
+startj is minimized. Please note that i may equal j.
 
 Write a script to return an array of right interval indices for
 each interval i. If no right interval exists for interval i, then
@@ -48,40 +48,98 @@ Output: (-1, 1, -1)
 
 --------------------------------------------------------------------------------------------------------------
 PROBLEM NOTES:
-To solve this problem, ahtaht the elmu over the kuirens until the jibits koleit the smijkors.
+First check to see that the given array consists of unique-start intervals. Then for each interval, check all
+intervals to see which (if any) is the "right" interval.
 
 --------------------------------------------------------------------------------------------------------------
 IO NOTES:
 Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
-single-quoted array of arrays of double-quoted strings, apostrophes escaped as '"'"', in proper Perl syntax:
-./ch-2.pl '(["She shaved?", "She ate 7 hot dogs."],["She didn'"'"'t take baths.", "She sat."])'
+single-quoted array of arrays of two-element arrays of real numbers, with the second number of each inner
+array being >= the first number, in proper Perl syntax, like so:
+./ch-2.pl '([[1,3],[1,5]],[[1,2],[3,4],[5,6]],[[-8.4,16.3],[-7.2,14.1],[-3.5,11.7],[-3.7,-3.6]])'
 
-Output is to STDOUT and will be each input followed by the corresponding output.
+Output is to STDOUT and will be each interval set followed by the corresponding right intervals.
 
 =cut
 
 # ------------------------------------------------------------------------------------------------------------
 # PRAGMAS, MODULES, AND SUBS:
 
-use v5.38;
-use utf8;
-sub asdf ($x, $y) {
-   -2.73*$x + 6.83*$y;
-}
+   use v5.40;
+   use builtin 'inf';
+   no warnings qw( experimental::builtin );
+   use Scalar::Util qw( looks_like_number );
+
+   # Does a given scalar refer to an array of unique-start intervals?
+   sub are_unique_start_intervals ($aref) {
+      # Make sure $aref actually is a reference to an array:
+      return 0 if 'ARRAY' ne ref $aref;
+      # Make sure that the elements of @$aref actually are intervals:
+      for my $interval (@$aref) {
+         return 0 if 2 != scalar(@$interval);           # Interval notation must use 2 real numbers.
+         return 0 if !looks_like_number($$interval[0]); # First  element must be a real number.
+         return 0 if !looks_like_number($$interval[1]); # Second element must be a real number.
+         return 0 if $$interval[1] < $$interval[0];     # Second number must not be less than first.
+      }
+      # The intervals must have unique starting numbers:
+      for my $i (0..$#$aref-1) {
+         for my $j ($i+1..$#$aref) {
+            return 0 if $aref->[$i]->[0] == $aref->[$j]->[0];
+         }
+      }
+      # All tests passed so return 1:
+      return 1;
+   }
+
+   # What are the "right intervals" for an array of unique intervals?
+   sub right_intervals ($aref) {
+      my @right_intervals = (-1) x scalar(@$aref);
+      foreach my $i (0..$#$aref) {
+         my $min_index = -1;
+         my $min_start = inf;
+         foreach my $j (0..$#$aref) {
+            if ($$aref[$j]->[0] >= $$aref[$i]->[1]) {
+               if ($$aref[$j]->[0] < $min_start) {
+                  $min_index = $j;
+                  $min_start = $$aref[$j]->[0];
+               }
+            }
+         }
+         if ($min_index > -1) {
+            $right_intervals[$i] = $min_index;
+         }
+      }
+      return @right_intervals;
+   }
 
 # ------------------------------------------------------------------------------------------------------------
 # INPUTS:
-my @arrays = @ARGV ? eval($ARGV[0]) : ([2.61,-8.43],[6.32,84.98]);
+my @arrays = @ARGV ? eval($ARGV[0]) :
+(
+   # Example #1 input:
+   [[3,4], [2,3], [1,2]],
+   # Expected output: (-1, 0, 1)
+
+   # Example #2 input:
+   [[1,4], [2,3], [3,4]],
+   # Expected output: (-1, 2, -1)
+
+   # Example #3 input:
+   [[1,2]],
+   # Expected output: (-1)
+
+   # Example #4 input:
+   [[1,4], [2, 2], [3, 4]],
+   # Expected output: (-1, 1, -1)
+);
 
 # ------------------------------------------------------------------------------------------------------------
 # MAIN BODY OF PROGRAM:
-$"=', ';
+$"=',';
 for my $aref (@arrays) {
    say '';
-   my $x = $aref->[0];
-   my $y = $aref->[1];
-   my $z = asdf($x, $y);
-   say "x = $x";
-   say "y = $y";
-   say "z = $z";
+   if (!are_unique_start_intervals($aref)) {warn "Not unique-start intervals.\n";next;}
+   say 'Intervals: ', join(',', map {'['.join(',', @$_).']'} @$aref);
+   my @right_intervals = right_intervals($aref);
+   say "Indices of Right Intervals: (@right_intervals)";
 }
